77618b25ef999c1571ab80e57d72e55005469f2c
arch <beyondthestars628@gmail.com>
2023-09-12 09:03:52 -0400

    update

--- Homework/9⁄11⁄23 HW - Write a dart program to solve quadratic equation/quadratic.dart
+++ Homework/9⁄11⁄23 HW - Write a dart program to solve quadratic equation/quadratic.dart
@@ -1,42 +1,32 @@
-
-import 'dart:io';
 import 'dart:math';
 
 void main() {
   // form ax^2 + bx + c = 0
-  
-  print("enter value a: ");
-  int a = int.parse(stdin.readLineSync()!);
-  print("enter value b:");
-  int b = int.parse(stdin.readLineSync()!);
-  print("enter value c");
-  int c = int.parse(stdin.readLineSync()!);
+
+  const double a = 1;
+  const double b = 2;
+  const double c = 1;
 
   List solutions = GetSolutions(a, b, c);
 
-  if(solutions.isEmpty)
-  {
+  if (solutions.isEmpty) {
     print("no real solutions.");
     return;
   }
 
   print("all real solutions: ");
-  for(num n in solutions)
-  {
+  for (num n in solutions) {
     print(n);
   }
 }
 
-List GetSolutions(int a, int b, int c)
-{
-
-  if(pow(b, 2) - 4*a*c < 0)
-  {
+List GetSolutions(double a, double b, double c) {
+  if (pow(b, 2) - 4 * a * c < 0) {
     return [];
   }
 
-  double x1 = (-b + sqrt(pow(b, 2) - 4*a*c))/(2*a);
-  double x2 = (-b - sqrt(pow(b, 2) - 4*a*c))/(2*a);
+  double x1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+  double x2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
 
   return [x1, x2];
 }
